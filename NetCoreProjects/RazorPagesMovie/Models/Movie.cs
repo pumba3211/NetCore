ffaf64b6ef0850248bbf9fb3a413e2a7077b5deb
Ronald Acuña Arias <ronald.arias@forsyteit.com>
2021-08-01 15:45:17 -0600

    Use project guide for .net core

---
+++ NetCoreProjects/RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int ID { set; get; }
+        public string Title { set; get; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { set; get; }
+        public string Genre { set; get; }
+
+        public decimal Price { set; get; }
+    }
+}
