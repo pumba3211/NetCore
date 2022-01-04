dc7cdcee2ee6d9ef80c67827025ef33be8030db4
Ronald Acuña Arias <ronald.arias@forsyteit.com>
2022-01-03 20:55:03 -0600

    Mi dieta project creation

---
+++ MiDieta/MiDietaApp/MiDietaApp/MiDietaApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MiDietaApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace MiDietaApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ MiDieta/MiDietaApp/MiDietaApp/MiDietaApp/Views/ItemsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using MiDietaApp.Models;
+using MiDietaApp.ViewModels;
+using MiDietaApp.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MiDietaApp.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel _viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
